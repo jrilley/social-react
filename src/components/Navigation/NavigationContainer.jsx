7196e43dd4985a050597fdd2d719d211c2b50b06
jrilley <j.rilley01@gmail.com>
2021-02-01 23:56:42 +0300

    Fixed bugs

---
+++ src/components/Navigation/NavigationContainer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import s from './Navigation.module.css';
+import {NavLink} from "react-router-dom";
+import Friends from "./Friends/Friends";
+import StoreContext from '../../StoreContext';
+
+const Navigation = () => {
+
+
+    return (<StoreContext>
+            {store => {
+                let friendsElements = store.getState().friendsPage.friends.map(friend =>
+                    <Friends id={friend.id} name={friend.name} src={friend.img}/>);
+
+                return <nav className={s.nav}>
+                    <div className={s.item}>
+                        <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
+                    </div>
+                    <div className={s.item}>
+                        <NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink>
+                    </div>
+                    <div className={s.item}>
+                        <NavLink to="/news" activeClassName={s.active}>News</NavLink>
+                    </div>
+                    <div className={s.item}>
+                        <NavLink to='/music' activeClassName={s.active}>Music</NavLink>
+                    </div>
+                    <div className={s.item}>
+                        <NavLink to='/settings' activeClassName={s.active}>Settings</NavLink>
+                    </div>
+
+                    <div className={s.item}>
+                        <h3>Friends</h3>
+                        {friendsElements}
+                    </div>
+                </nav>
+            }
+            }
+        </StoreContext>
+
+    );
+}
+
+export default Navigation;
